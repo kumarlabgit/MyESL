0c6c743e7a0905fcd1deb8267ab8dae33ebba34e
msanderford <maxwell.sanderford@gmail.com>
2024-05-07 10:26:07 -0700

    Various bug fixes

--- build_script.bat
+++ build_script.bat
@@ -1,11 +1,11 @@
 rm -rf bin
 mkdir bin
 cd src
-<mingw_path>\mingw64\bin\g++ -std=c++17 preprocess_main.cpp preprocess.cpp -o preprocess -static
-<mingw_path>\mingw64\bin\g++ -std=c++17 sg_lasso_main.cpp sg_lasso.cpp -static libopenblas.dll -static liblapack.dll -o sg_lasso -Iinclude -static -lpthread
-<mingw_path>\mingw64\bin\g++ -std=c++17 sg_lasso_leastr_main.cpp sg_lasso_leastr.cpp -static libopenblas.dll -static liblapack.dll -o sg_lasso_leastr -Iinclude -static -lpthread
-<mingw_path>\mingw64\bin\g++ -std=c++17 overlapping_sg_lasso_leastr_main.cpp overlapping_sg_lasso_leastr.cpp -static libopenblas.dll -static liblapack.dll -o overlapping_sg_lasso_leastr -Iinclude -static -lpthread
-<mingw_path>\mingw64\bin\g++ -std=c++17 overlapping_sg_lasso_logisticr_main.cpp overlapping_sg_lasso_logisticr.cpp -static libopenblas.dll -static liblapack.dll -o overlapping_sg_lasso_logisticr -Iinclude -static -lpthread
+..\..\mingw64\bin\g++ -std=c++17 preprocess_main.cpp preprocess.cpp -o preprocess -static -lstdc++fs
+..\..\mingw64\bin\g++ -std=c++17 sg_lasso_main.cpp sg_lasso.cpp -static libopenblas.dll -static liblapack.dll -o sg_lasso -Iinclude -static -lpthread
+..\..\mingw64\bin\g++ -std=c++17 sg_lasso_leastr_main.cpp sg_lasso_leastr.cpp -static libopenblas.dll -static liblapack.dll -o sg_lasso_leastr -Iinclude -static -lpthread
+..\..\mingw64\bin\g++ -std=c++17 overlapping_sg_lasso_leastr_main.cpp overlapping_sg_lasso_leastr.cpp -static libopenblas.dll -static liblapack.dll -o overlapping_sg_lasso_leastr -Iinclude -static -lpthread
+..\..\mingw64\bin\g++ -std=c++17 overlapping_sg_lasso_logisticr_main.cpp overlapping_sg_lasso_logisticr.cpp -static libopenblas.dll -static liblapack.dll -o overlapping_sg_lasso_logisticr -Iinclude -static -lpthread
 mv preprocess.exe sg_lasso.exe sg_lasso_leastr.exe overlapping_sg_lasso_leastr.exe overlapping_sg_lasso_logisticr.exe ../bin
 cp *.dll ..\bin
 cp *.lib ..\bin
@@ -16,3 +16,4 @@ mv dist\ESL_pipeline\_internal .
 mv dist\ESL_pipeline\ESL_pipeline.exe .
 mv dist\ESL_model_apply\ESL_model_apply.exe .
 rm -rf build
+rm -rf dist
